2361966214c5dfbf22005a94017bee2648c216fb
Aileena <aile0074@stud.kea.dk>
2023-09-22 09:02:56 +0200

    good commit message

---
+++ src/main/java/kea/Main.java
@@ -0,0 +1,17 @@
+package kea;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Why do Java developers wear glasses?");
+        System.out.println("Because they don't C#!");
+
+        System.out.println("   ******   ");
+        System.out.println(" *        * ");
+        System.out.println(" *  O   O  * ");
+        System.out.println(" *    ^    * ");
+        System.out.println(" *  \\___/  * ");
+        System.out.println("  *       *  ");
+        System.out.println("    *****    ");
+        System.out.println(" HAHAHAHAHA! ");
+    }
+}
